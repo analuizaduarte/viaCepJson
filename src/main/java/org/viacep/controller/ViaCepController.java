7d9ac8eebbd4ceabd9dd147d40761d683aa2ca2b
analuizaduarte <analuiisa29@gmail.com>
2024-09-21 16:11:46 -0300

    Criação do ViaCepController

---
+++ src/main/java/org/viacep/controller/ViaCepController.java
@@ -0,0 +1,40 @@
+package org.viacep.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.viacep.entity.dto.response.CepResponse;
+
+@RestController
+@RequestMapping("/api")
+public class ViaCepController {
+
+    private WebClient webClient;
+
+    @Autowired
+    public ViaCepController(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.baseUrl("https://viacep.com.br/").build();
+    }
+
+        /*@GetMapping("/{cep}/json")
+        public CepResponse getCep(@PathVariable Integer cep){
+            return this.webClient.get()
+                    .uri("/ws/{cep}/json", cep)
+                    .retrieve()
+                    .bodyToMono(CepResponse.class)
+                    .block();
+        }*/
+        @GetMapping("/consulta-cep/{cep}")
+        public CepResponse getCep(@PathVariable Integer cep){
+            return this.webClient.get()
+                    .uri("/ws/{cep}/json", cep)
+                    .retrieve()
+                    .bodyToMono(CepResponse.class)
+                    .block();
+        }
+
+
+}
