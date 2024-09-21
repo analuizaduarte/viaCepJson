ddbc8d8b9439d69d58cfe85b9adc4fae1ac6b9ef
analuizaduarte <analuiisa29@gmail.com>
2024-09-21 16:10:00 -0300

    Inserção do @JsonAlias no CepResponse

--- src/main/java/org/viacep/entity/dto/response/CepResponse.java
+++ src/main/java/org/viacep/entity/dto/response/CepResponse.java
@@ -1,5 +1,6 @@
 package org.viacep.entity.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -9,8 +10,10 @@ import lombok.Setter;
 @Setter
 public class CepResponse {
     private String cep;
+    @JsonAlias("logradouro")
     private String rua;
     private String bairro;
+    @JsonAlias("localidade")
     private String cidade;
     private String uf;
 }
